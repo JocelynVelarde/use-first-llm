1ccfbc9fc1a7e8c0f510a224f04467af38c69591
JocelynVelarde <a01285780@itesm.mx>
2025-01-12 16:47:41 -0600

    first completion

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,11 +1,13 @@
 # This file will serve as our GPT connection
 
+api_key = "xxxxx"
+
 from openai import OpenAI
-client = OpenAI()
+client = OpenAI(api_key=api_key)
 completion = client.chat.completions.create(
     model="gpt-4o",
-    store=True,
     messages=[
         {"role": "user", "content": "write a haiku about ai"}
     ]
 )
+print(completion.choices[0].message.content)
