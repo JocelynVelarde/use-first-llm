dcc045d528bb4e06c65c1e56d1ead5ed1d9e6f3f
JocelynVelarde <a01285780@itesm.mx>
2025-01-12 18:34:41 -0600

    renamed

--- home.py
+++ home.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from gpt_wrapper import generate_image, generate_text
+from gemini_wrapper import generate_gemini_text
 
 st.title("Welcome to my first LLM requests")
 
 st.header("OpenAI API")
 open_ai_prompt = st.text_input("Please type your prompt")
 if st.button("Send"):
-    # gpt method
+    generate_text(open_ai_prompt)
     st.success("Content generated!")
 else: 
     st.warning("Please insert a prompt")
@@ -15,7 +17,7 @@ st.divider()
 st.header("Gemini API")
 gemini_prompt = st.text_input("Please type your prompt", key=1)
 gemini_tokens = st.number_input("Please select number of tokens")
-if st.button("Send", key=1):
+if st.button("Send", key=2):
     # gpt method
     st.success("Content generated!")
 else: 
